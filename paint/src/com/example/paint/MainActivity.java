050c1e3c10b04aad075ed9de61137377810e99fe
sameer <sameersamisa@gmail.com>
2014-06-30 17:13:17 -0400

    PAINT

--- paint/src/com/example/paint/MainActivity.java
+++ paint/src/com/example/paint/MainActivity.java
@@ -1,54 +1,78 @@
 package com.example.paint;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.widget.ImageView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private final int DIALOG_SINGLE_CHOICE=0;	
-	//ImageView imgView; 
+	private final int DIALOG_SINGLE_CHOICE = 0;
+
 	TextView test;
-	int color=0;
+	int color = 0;
 	canvas drawing;
-	
+	ImageButton create;
+	ImageButton brush;
+	ImageButton save;
+	ImageButton eraser;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		drawing=(canvas)findViewById(R.id.canvas);
-		
-		//imgView = (ImageView)findViewById(R.id.imageview);
-		//imgView.setBackgroundResource(R.drawable.visiblespectrum);
-		
-		test = (TextView)findViewById(R.id.test);
-	}
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		/*try{
-		
-		imgView.setDrawingCacheEnabled(true);
-		Bitmap temBitmap = imgView.getDrawingCache();
-		color= temBitmap.getPixel((int)event.getX(), (int)event .getY());
-		setpaintcolor();
-		
-		} catch(Exception e) {
-			
-		}*/
-		
-		
-		return super.onTouchEvent(event);
-	}
+		drawing = (canvas) findViewById(R.id.canvas);
 
-	private void setpaintcolor() {
-		// TODO Auto-generated method stub
-		drawing.setpaintcolor(color);
-		Integer newColor = color;
-		test.setText(newColor.toString());
-	}
 	
+		create = (ImageButton) findViewById(R.id.create);
+		brush = (ImageButton) findViewById(R.id.brush);
+		eraser = (ImageButton) findViewById(R.id.brush);
+		save = (ImageButton) findViewById(R.id.save);
+		save.setOnClickListener(new OnClickListener() {
+			AlertDialog.Builder savedialog = new AlertDialog.Builder(MainActivity.this);
+			@Override
+			public void onClick(View v) {
+				
+
+			}
+
+		});
+		eraser.setOnClickListener(new OnClickListener() {
+			
+
+			@Override
+			public void onClick(View v) {
+				Dialog eraserdialog = new Dialog(MainActivity.this);
+				eraserdialog.setTitle("Eraser Size");
+				eraserdialog.setContentView(R.layout.eraser_chooser);
+			eraserdialog.show();}
+
+		});
+		
+		brush.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Dialog brushdialog = new Dialog(MainActivity.this);
+				brushdialog.setTitle("Brush Size");
+				brushdialog.setContentView(R.layout.brush_chooser);
+			brushdialog.show();}
+
+		});
+		create.setOnClickListener(new OnClickListener() {
+			AlertDialog.Builder createdialog = new AlertDialog.Builder(MainActivity.this);
+
+			@Override
+			public void onClick(View arg0) {
+
+			}
+
+		});
+	}
+
 }
